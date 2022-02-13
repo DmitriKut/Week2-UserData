440696628b06deff3f296a680308efd4e3566b4a
Dmitri <dimaskut@gmail.com>
2022-02-13 16:54:11 +0200

    Control Flow

---
+++ UseAge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UseAge
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//programm küsib kasutaja vanust;
+			//programm arvutab kasutaja sünniaastat;
+			//programm kuvab sünniaastat konsoolis
+			//"oled sündinud aastal {yearOfBirth}"
+
+			Console.WriteLine("Palun, sisesta oma vanus:");
+			int userAge = Convert.ToInt32(Console.ReadLine());
+
+			int yearOfBirth = 2021 - userAge;
+
+			//Console.WriteLine("Oled " + userAge + " aastat vana.");
+
+			//string interpolation
+
+			Console.WriteLine($"Oled {yearOfBirth} aastal sündinud.");
+		}
+	}
+}
